221af0b5144c8130f06960038912de067a9d9e54
poulpyR2r <guewencarre@gmail.com>
2023-11-14 11:18:28 +0100

    update app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const postRoutes = require("../src/api/route/postRoutes");
 
-const hostname = "0.0.0.0";
 const port = 5555;
 const app = express();
 
@@ -22,9 +21,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/compteRendu")
     console.log("Database connected");
 
     // Start the server after successful database connection
-    app.listen(port, hostname, () => {
-      console.log(`Example app listening at http://${hostname}:${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
   })
   .catch(err => {
     console.error("Database connection failed", err);
